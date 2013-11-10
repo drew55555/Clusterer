4adf344bc9afd318216c6857b446175f6b89e6d6
drew55555 <drew.dishman@yahoo.com>
2013-11-10 01:42:05 -0800

    using dot product for similarity and finds most popular topic

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,34 +4,93 @@ import scala.collection.immutable._
 import scala.math._
 import scala.annotation.tailrec
 import sun.org.mozilla.javascript.ast.Yield
+import org.bson._
+import javax.xml.soap.Text
+import sun.font.TrueTypeFont
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-    val query = MongoDBObject("CountryCode" -> "US")
-    val tweetNum = args(2).toInt
+    //val query = MongoDBObject("CountryCode" -> "US")
+    val nelat = 41
+    val nelon = -73
+    val swlat = 40
+    val swlon = -74
+    val nwlat = nelat
+    val nwlon = swlon
+    val selat = swlat
+    val selon = nelon
+    val geo = MongoDBObject("$geometry" ->
+      MongoDBObject("type" -> "Polygon",
+        "coordinates" -> List(((GeoCoords(nelon, nelat),
+          GeoCoords(selon, selat),
+          GeoCoords(swlon, swlat),
+          GeoCoords(nwlon, nwlat),
+          GeoCoords(nelon, nelat))))))
+    val query = "Location" $geoWithin (geo)
     val factory = new MongoFactory(args(0), args(1))
-    val idf = createIDF(factory.collection, query)
     val tweets = getTweets(factory.collection, query)
-    val first = tweets(tweetNum)
-    val dists = for (x <- tweets.slice(tweetNum + 1, tweets.length))
-      yield Tuple2(x, tweetDist(first, x, idf))
-    val minDist = dists.sortBy(x => x._2)
-    println(first.Text)
-    println
-    minDist.take(25).foreach(x => println(x._1.Text + "\t" + x._2))
-  }
-  def getTweets(mongo: MongoCollection, query: MongoDBObject): List[Tweet] = {
-    val res = for(x <- mongo.find(query)) yield buildTweet(x)
-    res.toList
+    val count = tweets.count(x => true)
+    println(count + " Tweets Read")
+    val idf = createIDF(tweets.values, count)
+    println("IDF Created")
+    findNeighboors(tweets.values, idf)
+    println("Neighboors found")
+    countReverseNeighboors(tweets)
+    println("Reverse Neighboors Counted")
+
+    try {
+      val popular = tweets.maxBy(x => x._2.revNearCount)._2
+      println(popular.Text)
+      println
+      println
+      for (ID <- popular.nearestNeighbors) {
+        println(tweets.get(ID._1).get.Text + "\t" + ID._2)
+
+      }
+    } catch {
+      case e: Exception =>
+        println("Exception")
+      // TODO: handle exception
+    }
+
+    //    val first = tweets(tweetNum)
+    //    val dists = for (x <- tweets.slice(tweetNum + 1, tweets.length))
+    //      yield Tuple2(x, tweetDotProd(first, x, idf))
+    //    val minDist = dists.sortBy(x => x._2)
+    //    println(first.Text)
+    //    println
+    //    minDist.takeRight(25).foreach(x => println(x._1.Text + "\t" + x._2))
   }
 
-  def createIDF(collection: MongoCollection, query: MongoDBObject): Map[String, Double] = {
+  def countReverseNeighboors(tweets: Map[String, Tweet]): Unit = {
+    for (
+      tweet <- tweets.values;
+      neighboor <- tweet.nearestNeighbors
+    ) {
+      tweets.get(neighboor._1).get.revNearCount += 1
+    }
+  }
+
+  def findNeighboors(tweets: scala.collection.Iterable[Tweet], idfResult: Map[String, Double]): Unit = {
+    for (tweet <- tweets) {
+      var dists = (for (
+        tempTweet <- tweets if (tweet.ID != tempTweet.ID)
+      ) yield new Tuple2(tempTweet.ID, tweetDotProd(tweet, tempTweet, idfResult))).toList
+      dists = dists.sortBy(x => x._2)
+      tweet.nearestNeighbors ++= dists.takeRight(10)
+    }
+  }
+
+  def getTweets(mongo: MongoCollection, query: MongoDBObject): Map[String, Tweet] = {
+    val res = for (x <- mongo.find(query)) yield buildTweet(x)
+    res.toMap
+  }
+
+  def createIDF(tweets: scala.collection.Iterable[Tweet], count: Int): Map[String, Double] = {
     val words = scala.collection.mutable.ArrayBuffer[String]()
-    val tweets = collection.find(query)
-    val docCount = tweets.count
-    tweets.foreach(x => words ++= buildTweet(x).termFreq.keys)
-    idfMap(words.groupBy(x => x), docCount) //.mapValues(x => idf(x.size, docCount))
+    tweets.foreach(x => words ++= x.termFreq.keys)
+    idfMap(words.groupBy(x => x), count) //.mapValues(x => idf(x.size, docCount))
   }
 
   def idfMap(elements: Map[String, scala.collection.mutable.ArrayBuffer[String]], docCount: Double): Map[String, Double] = {
@@ -42,7 +101,18 @@ object Main {
     log(docCount / numTerms)
   }
 
-  
+  def tweetDotProd(tweet1: Tweet, tweet2: Tweet, idfResult: Map[String, Double]): Double = {
+    val wordSet = (tweet1.termFreq.keys ++ tweet2.termFreq.keys).toList
+    @tailrec
+    def dotProd(words: List[String], accu: Double): Double = {
+      words match {
+        case word :: tail => dotProd(tail, accu + ((tweet1.termFreq.getOrElse[Int](word, 0) * idfResult.getOrElse[Double](word, 0)) * (tweet2.termFreq.getOrElse[Int](word, 0) * idfResult.getOrElse[Double](word, 0))))
+        case _ => accu
+      }
+    }
+    dotProd(wordSet, 0)
+  }
+
   def tweetDist(tweet1: Tweet, tweet2: Tweet, idfResult: Map[String, Double]): Double = {
     val wordSet = (tweet1.termFreq.keys ++ tweet2.termFreq.keys).toList
     @tailrec
@@ -59,13 +129,14 @@ object Main {
     pow(x - y, 2)
   }
 
-  def buildTweet(obj: MongoDBObject): Tweet = {
+  def buildTweet(obj: MongoDBObject): (String, Tweet) = {
+    val id = obj.getAs[types.ObjectId]("_id").get
     val text = obj.getAs[String]("Text").get;
     val location = obj.getAs[BasicDBObject]("Location").get
     val coordinates = location.getAs[MongoDBList]("coordinates").get
     val date = obj.getAs[Date]("TweetTime").get
     val hashtags = obj.getAs[MongoDBList]("HashTags").get.toList
-    new Tweet(text, coordinates, date, hashtags)
+    new Tuple2(id.toString(), new Tweet(id.toString(), text, coordinates, date, hashtags))
   }
 
 }
--- src/main/scala/Tweet.scala
+++ src/main/scala/Tweet.scala
@@ -1,14 +1,18 @@
 import com.github.nscala_time.time.Imports._
 import com.mongodb.casbah.Imports._
-import java.util.Date;
+import java.util.Date
 import scala.collection.immutable.Map
 import scala.collection.mutable._
+import org.bson._
 
-class Tweet(text: String, location: MongoDBList, date: Date, hashtags: List[Any]) {
+class Tweet(id: String, text: String, location: MongoDBList, date: Date, hashtags: List[Any]) {
 
+  val ID: String = id.toString()
   val Text: String = text
   val Location: MongoDBList = location
   val Date: Date = date
+  val nearestNeighbors : ListBuffer[(String, Double)] = ListBuffer[(String, Double)]()
+  var revNearCount = 0
 
   val termFreq: scala.collection.immutable.Map[String, Int] = {
     val words = ArrayBuffer[String]()
